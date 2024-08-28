dd273b03060bdf0ac16f9f1973c7847277d350ec
Aline Areda <aredaaline@gmail.com>
2024-08-28 19:29:00 -0300

    Update multer.ts

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,21 +1,15 @@
 import crypto from 'crypto';
-import multer from "multer";
-
-import { extname, resolve } from 'path';
+import multer from 'multer';
 
 export default {
-    upload(folder: string) {
+    upload() {
         return {
-            storage: multer.diskStorage({
-                destination: resolve(__dirname, '..', '..', folder),
-                filename: (request, file, callback) => {
-                    const fileHash = crypto.randomBytes(16).toString("hex");
-                    const fileName = `${fileHash}.${file.originalname}`
-
-                    return callback(null, fileName)
-                }
-            })
-        }
-
+            storage: multer.memoryStorage(),
+            filename: (request, file, callback) => {
+                const fileHash = crypto.randomBytes(16).toString('hex');
+                const fileName = `${fileHash}-${file.originalname}`;
+                callback(null, fileName);
+            }
+        };
     }
-}
+};
