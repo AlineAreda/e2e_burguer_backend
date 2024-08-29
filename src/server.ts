412a1a9c522f3f0db7ecf705569484f817c4f668
Aline Areda <aredaaline@gmail.com>
2024-08-29 18:19:58 -0300

    Update swagger

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,7 @@ import path from 'path';
 import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
 
-import swaggerDocs from "./swagger.json";
-
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
@@ -15,28 +14,28 @@ app.use(cors());
 
 app.use(router);
 
-app.use('/api-swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-
+app.use('/api-swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(
     '/files',
     express.static(path.resolve(__dirname, '..', 'tmp'))
-)
+);
 
-app.use((err: Error, req: Request, res: Response, nex: NextFunction) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {  // Corrigido o nome 'next'
     if (err instanceof Error) {
         return res.status(400).json({
             error: err.message
-        })
+        });
     }
 
     return res.status(500).json({
         status: 'error',
-        mesage: 'Internal server error.'
-    })
+        message: 'Internal server error.'  // Corrigido o erro de digitação 'mesage' para 'message'
+    });
 
-})
+});
 
-//apenas para rodar local o listen
-//app.listen(3333, () => console.log('Servidor está rodando em http://localhost:3333,'));
+// Apenas para rodar localmente
+// app.listen(3333, () => console.log('Servidor está rodando em http://localhost:3333,'));
+
 export default app;  // Exportando o app para que Vercel o utilize como uma função serverless
