1eea6eb9f1f747d37ccb1c5d1cb43969ee9b4743
Aline Areda <aredaaline@gmail.com>
2024-10-11 16:16:36 -0300

    Alteração cors

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,13 @@ const swaggerDocs = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+// Configuração do CORS
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
