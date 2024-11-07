e796616c00d8fc77f56bd9497c56d49b424a116b
Aline Areda <aredaaline@gmail.com>
2024-11-07 14:15:18 -0300

    ajuste em isAuthenticated

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -12,28 +12,28 @@ export function isAuthenticated(
 ) {
     const authToken = req.headers.authorization;
 
+    // Verifica se o token de autenticação está presente
     if (!authToken) {
-        return res.status(401).end();
+        return res.status(401).json({ error: "Acesso Negado. Realize login" });
     }
 
     const [, token] = authToken.split(' ');
 
     try {
+        // Verifica o token JWT
         const { sub } = verify(
             token,
             process.env.JWT_SECRET
         ) as Payload;
 
-        
+        // Define o ID do usuário no objeto `req` para acesso posterior
         req.user_id = sub;
 
         console.log('User ID from token:', req.user_id); // Verificação
 
         return next();
     } catch (err) {
-      console.error('Token verification failed:', err);
-        return res.status(401).end();
-
+        console.error('Token verification failed:', err);
+        return res.status(401).json({ error: "Acesso Negado. Realize login" });
     }
-
 }
